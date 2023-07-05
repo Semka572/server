5f9d4e29e3bb5e7e305f2d39bbc0c8647a3277f8
Semka572 <semen.kurta@gmail.com>
2023-07-05 18:32:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) =>{
-  res.send({ hi: 'there'});
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
 });
 
-const PORT = precess.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
