a12a993d05a330efc656a16bab2c24022d5f3d05
Semka572 <semen.kurta@gmail.com>
2023-07-13 22:54:11 +0300

    changed  logout and currents_user url's to default

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,12 +14,12 @@ module.exports = app => {
   app.get('/auth/google/callback', passport.authenticate('google'));
 
   
-  app.get('/api/gog/logout', (req, res) => {
+  app.get('/api/logout', (req, res) => {
     req.logout();
     res.send(req.user);
   });
 
-  app.get('/api/gog/current_user', (req, res) => {
+  app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
 };
