5374a05fb9f9714927769eab86c09626f0d37213
Semka572 <semen.kurta@gmail.com>
2023-07-18 22:33:14 +0300

    Created server on front, modules added
    Created server, added module to start both servers concurrently,  added proxy

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Darova friki
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Kto?
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
